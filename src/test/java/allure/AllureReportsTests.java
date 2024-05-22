a75c8a0cbce98b06c59c2e13fa77f83c245706dd
IIvanov57 <ivanov080892@mail.ru>
2024-05-22 18:47:13 +0300

    fix gitignore

--- src/test/java/allure/AllureReportsTests.java
+++ src/test/java/allure/AllureReportsTests.java
@@ -1,2 +1,74 @@
-package allure;public class AllureReportsTests {
+package allure;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.EventsCollector;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+
+public class AllureReportsTests {
+
+
+  @Test
+  @DisplayName("Проверка наличия ошибки в списке")
+  void checkIssueWithCleanSelenideTest() {
+    open("https://github.com/");
+
+    $(".header-search-button").click();
+    $("#query-builder-test").click();
+    $("#query-builder-test").setValue("Java").pressEnter();
+
+
+    $("[data-testid='results-list']").$(".search-title").click();
+
+    $("#issues-tab").click();
+
+    $(withText("#5164")).should(Condition.exist);
+  }
+
+  @Test
+  void checkIssueLambdaTest() {
+    SelenideLogger.addListener("allure", new EventsCollector());
+
+    step("Открываем страницу github", () -> {
+      open("https://github.com/");
+    });
+
+    step("Вводим в поисковик слово 'Java'", () -> {
+      $(".header-search-button").click();
+      $("#query-builder-test").click();
+      $("#query-builder-test").setValue("Java").pressEnter();
+    });
+    step("Ищем первый проект в списке", () -> {
+      $("[data-testid='results-list']").$(".search-title").click();
+    });
+
+    step("Преходим в раздел issue", () -> {
+      $("#issues-tab").click();
+    });
+
+    step("Проверяем наличе issue #5164", () -> {
+      $(withText("#5164")).should(Condition.exist);
+    });
+  }
+
+  @Test
+  void checkIssueWithStepsTest() {
+    SelenideLogger.addListener("allure", new EventsCollector());
+
+    IssueSteps steps = new IssueSteps();
+
+    steps.openPage();
+    steps.inputWord();
+    steps.searchFirstProject();
+    steps.clickIssueTab();
+    steps.checkIssue();
+  }
+
+
 }
--- src/test/java/allure/IssueSteps.java
+++ src/test/java/allure/IssueSteps.java
@@ -1,2 +1,38 @@
-package allure;public class IssueSteps {
+package allure;
+
+import com.codeborne.selenide.Condition;
+import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class IssueSteps {
+
+  @Step("Открываем страницу github")
+  public void openPage() {
+    open("https://github.com/");
+  }
+
+  @Step("Вводим в поисковик слово 'Java'")
+  public void inputWord() {
+    $(".header-search-button").click();
+    $("#query-builder-test").click();
+    $("#query-builder-test").setValue("Java").pressEnter();
+  }
+
+  @Step("Ищем первый проект в списке")
+  public void searchFirstProject() {
+    $("[data-testid='results-list']").$(".search-title").click();
+  }
+
+  @Step("Преходим в раздел issue")
+  public void clickIssueTab() {
+    $("#issues-tab").click();
+  }
+
+  @Step("Проверяем наличе issue #5164")
+  public void checkIssue() {
+    $(withText("#5164")).should(Condition.exist);
+  }
 }
